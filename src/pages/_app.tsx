e3f091c8b8a45e9b9bc551875dd7f80f82778e9a
ryo.watanabe <naberyo34@gmail.com>
2020-02-18 14:52:44 +0900

    add: styled-component導入
    - add: トップページのマークアップ PC版のみ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,38 @@
-import '../styles/global.css'
-import ExtLink from '../components/ext-link'
+import 'minireset.css'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
+
+const color = {
+  text: '#333',
+  bg: '#f6d365',
+}
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+    font-family: 'Raleway', sans-serif;
+    line-height: 1;
+    color: ${color.text};
+  }
+`
 
 export default ({ Component, pageProps }) => (
   <>
+    <Head>
+      <title>SZMD</title>
+      <meta
+        name="description"
+        content="An example Next.js site using Notion for the blog"
+      />
+      <meta name="og:title" content="My Notion Blog" />
+      <meta name="twitter:site" content="@_ijjk" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Raleway:700i,900i&amp;display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <GlobalStyle />
     <Component {...pageProps} />
-
-    <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://zeit.co/new/project?template=https://github.com/ijjk/notion-blog/tree/master">
-        <img
-          src="https://zeit.co/button"
-          height={46}
-          width={132}
-          alt="deploy to ZEIT button"
-        />
-      </ExtLink>
-      <span>
-        or{' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
-        </ExtLink>
-      </span>
-    </footer>
   </>
 )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,67 +1,103 @@
 import Link from 'next/link'
-import Header from '../components/header'
-import ExtLink from '../components/ext-link'
-import Features from '../components/features'
-import GitHub from '../components/svgs/github'
-import sharedStyles from '../styles/shared.module.css'
+// import Header from '../components/header'
+// import ExtLink from '../components/ext-link'
+// import Features from '../components/features'
+// import GitHub from '../components/svgs/github'
+// import sharedStyles from '../styles/shared.module.css'
+
+import styled from 'styled-components'
+
+const color = {
+  text: '#333',
+  bg: '#f6d365',
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: ${color.bg};
+`
+
+const Content = styled.section`
+  width: 1000px;
+  text-align: center;
+`
+
+const Title = styled.h1`
+  position: relative;
+  left: -0.05em;
+  font-size: 20rem;
+  font-weight: 900;
+  letter-spacing: -0.15em;
+`
+const Address = styled.h2`
+  position: relative;
+  top: -1em;
+  font-size: 2.4rem;
+  font-weight: 700;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+`
+
+const Menu = styled.ul`
+  position: fixed;
+  bottom: 20px;
+  display: flex;
+`
+
+const MenuItem = styled.li`
+  font-size: 1.6rem;
+  font-weight: 700;
+  margin-left: 20px;
+  transition: 0.2s;
+  &:hover {
+    opacity: 0.4;
+  }
+  &:first-child {
+    margin-left: 0;
+  }
+`
+
+// 諸々の事情でLinkに直接スタイルを当てることができない
+const LinkText = styled.a`
+  color: inherit;
+  text-decoration: none;
+`
 
 export default () => (
-  <>
-    <Header titlePre="Home" />
-    <div className={sharedStyles.layout}>
-      <img
-        src="/zeit-and-notion.png"
-        height="85"
-        width="250"
-        alt="ZEIT + Notion"
-      />
-      <h1>My Notion Blog</h1>
-      <h2>
-        Blazing Fast Notion Blog with Next.js'{' '}
-        <ExtLink
-          href="https://github.com/zeit/next.js/issues/9524"
-          className="dotted"
-          style={{ color: 'inherit' }}
-        >
-          SSG
-        </ExtLink>
-      </h2>
-
-      <Features />
-
-      <div className="explanation">
-        <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://zeit.co">ZEIT</ExtLink>. It
-          leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
-
-        <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
-        </p>
-      </div>
-    </div>
-  </>
+  <Wrapper>
+    <Content>
+      <Title>SZMD</Title>
+      <Address>szmd.jp</Address>
+      <Nav>
+        <Menu>
+          <MenuItem>
+            <Link href="/about">
+              <LinkText href="/about">ABOUT</LinkText>
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/blog">
+              <LinkText href="/blog">BLOG</LinkText>
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/works">
+              <LinkText href="/works">WORKS</LinkText>
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <Link href="/contact">
+              <LinkText href="/contact">CONTACT</LinkText>
+            </Link>
+          </MenuItem>
+        </Menu>
+      </Nav>
+    </Content>
+  </Wrapper>
 )
