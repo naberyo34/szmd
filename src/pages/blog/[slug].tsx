11d9aa6c0d954303e1043fc33b1882d935330a21
ryo.watanabe <naberyo34@gmail.com>
2020-04-07 19:30:43 +0900

    fix: [slug]を最新版notion blogを参考に魔改造

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -14,17 +14,20 @@ import Base from '../../components/base';
 
 // Get the data for each blog post
 // getStaticProps, getStaticPathsはNextの機能
-export async function getStaticProps({ params: { slug } }) {
+export async function getStaticProps({ params: { slug }, preview }) {
   // load the postsTable so that we can get the page's ID
   const postsTable = await getBlogIndex();
   const post = postsTable[slug];
 
-  if (!post) {
+  // if we can't find the post or if it is unpublished and
+  // viewed without preview mode then we just redirect to /blog
+  if (!post || (post.Published !== 'Yes' && !preview)) {
     console.log(`Failed to find post for slug: ${slug}`);
 
     return {
       props: {
         redirect: '/blog',
+        preview: false,
       },
       revalidate: 5,
     };
@@ -32,12 +35,35 @@ export async function getStaticProps({ params: { slug } }) {
   const postData = await getPageData(post.id);
   post.content = postData.blocks;
 
+  for (let i = 0; i < postData.blocks.length; i += 1) {
+    const { value } = postData.blocks[i];
+    const { type, properties } = value;
+    if (type === 'tweet') {
+      const src = properties.source[0][0];
+      // parse id from https://twitter.com/_ijjk/status/TWEET_ID format
+      const tweetId = src.split('/')[5].split('?')[0];
+      if (!tweetId) continue;
+
+      try {
+        const res = await fetch(
+          `https://api.twitter.com/1/statuses/oembed.json?id=${tweetId}`
+        );
+        const json = await res.json();
+        properties.html = json.html.split('<script')[0];
+        post.hasTweet = true;
+      } catch (_) {
+        console.log(`Failed to get tweet embed for ${src}`);
+      }
+    }
+  }
+
   const { users } = await getNotionUsers(post.Authors || []);
   post.Authors = Object.keys(users).map(id => users[id].full_name);
 
   return {
     props: {
       post,
+      preview: preview || false,
     },
     revalidate: 10,
   };
@@ -46,9 +72,13 @@ export async function getStaticProps({ params: { slug } }) {
 // Return our list of blog posts to prerender
 export async function getStaticPaths() {
   const postsTable = await getBlogIndex();
+  // we fallback for any unpublished posts to save build time
+  // for actually published ones
 
   return {
-    paths: Object.keys(postsTable).map(slug => getBlogLink(slug)),
+    paths: Object.keys(postsTable)
+      .filter(post => postsTable[post].Published === 'Yes')
+      .map(slug => getBlogLink(slug)),
     fallback: true,
   };
 }
@@ -102,7 +132,7 @@ const Article = styled.article`
   }
 `;
 
-const RenderPost = ({ post, redirect }) => {
+const RenderPost = ({ post }) => {
   let listTagName: string | null = null;
   let listLastId: string | null = null;
   let listMap: {
@@ -114,21 +144,10 @@ const RenderPost = ({ post, redirect }) => {
     };
   } = {};
 
-  if (redirect) {
-    return (
-      <>
-        <Head>
-          <meta name="robots" content="noindex" />
-          <meta httpEquiv="refresh" content={`0;url=${redirect}`} />
-        </Head>
-      </>
-    );
-  }
-
   return (
     <Base>
       <Article>
-        {/* {post.Date && <Date>{getDateStr(post.Date)}</Date>} */}
+        {post.Date && <Date>{getDateStr(post.Date)}</Date>}
         <h1>{post.Page || ''}</h1>
         {(!post.content || post.content.length === 0) && (
           <p>コンテンツがありません</p>
