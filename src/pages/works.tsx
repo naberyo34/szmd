45748c3b95e4f08127948aa1549103771f15e8f4
ryo.watanabe <naberyo34@gmail.com>
2020-04-11 18:58:25 +0900

    add: WORKのカードデザイン

--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 import { postIsReady } from '../lib/blog-helpers';
 import getNotionUsers from '../lib/notion/getNotionUsers';
 import getWorksIndex from '../lib/notion/getWorksIndex';
 import Base from '../components/base';
+import { width } from '../lib/style';
 
 export async function getStaticProps() {
   const postsTable = await getWorksIndex();
@@ -38,21 +40,60 @@ export async function getStaticProps() {
   };
 }
 
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 64px auto 0;
+  @media (max-width: ${width.ipad}) {
+    flex-direction: column;
+  }
+`;
+
+const Card = styled.div`
+  width: calc(50% - 16px);
+  height: 200px;
+  margin-top: 32px;
+  overflow: hidden;
+  cursor: pointer;
+  border-radius: 8px;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+  transition: box-shadow 0.4s;
+  @media (max-width: ${width.ipad}) {
+    width: 100%;
+  }
+  &:hover {
+    box-shadow: 0 0 24px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: transform 0.4s;
+  ${Card}:hover & {
+    transform: scale(1.2, 1.2);
+  }
+`;
+
 const Works = ({ posts = [] }) => {
   return (
     <Base heading="WORKS">
       {posts.length === 0 && <p>投稿がありません</p>}
-      {posts.map(post => {
-        return (
-          // コンテンツの中身が空だとapiがnullになって画像が表示されないらしい
-          <img
-            key={post.id}
-            src={`/api/asset?assetUrl=${post.Thumbnail}&blockId=${post.id}`}
-            alt={post.Page}
-            width="100"
-          />
-        );
-      })}
+      <CardWrapper>
+        {posts.map(post => {
+          return (
+            // コンテンツの中身が空だとapiがnullになって画像が表示されないらしい
+            <Card key={post.id}>
+              <ThumbnailImage
+                src={`/api/asset?assetUrl=${post.Thumbnail}&blockId=${post.id}`}
+                alt={post.Page}
+              />
+            </Card>
+          );
+        })}
+      </CardWrapper>
     </Base>
   );
 };
