debed7d425f043dbc5875b57951d7b31a97d71f5
ryo.watanabe <naberyo34@gmail.com>
2020-04-27 20:57:47 +0900

    add: 環境をゼロから構築

--- src/components/ext-link.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ExtLink = props => (
-  <a {...props} rel="noopener" target={props.target || '_blank'} />
-);
-
-export default ExtLink;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,54 +1,17 @@
-import React from 'react';
-import styled from 'styled-components';
-import BaseComponent from '../components/base';
-import HeadComponent from '../components/head';
-import { width, color } from '../lib/style';
+import React from 'react'
+import Link from 'next/link'
+import Layout from '../components/Layout'
 
-const Author = styled.section`
-  max-width: 800px;
-  padding: 0 20px 40px 20px;
-  margin: 120px auto 0;
-  text-align: center;
-  background: ${color.gray};
-`;
+const AboutPage: React.FunctionComponent = () => (
+  <Layout title="About | Next.js + TypeScript Example">
+    <h1>About</h1>
+    <p>This is the about page</p>
+    <p>
+      <Link href="/">
+        <a>Go home</a>
+      </Link>
+    </p>
+  </Layout>
+)
 
-const AuthorIcon = styled.img`
-  position: relative;
-  top: -100px;
-`;
-
-const AuthorName = styled.h3`
-  margin-top: -100px;
-  font-family: 'Raleway', sans-serif;
-  font-size: 4.8rem;
-  font-style: italic;
-`;
-
-const AuthorDescription = styled.p`
-  margin-top: 40px;
-  font-size: 1.6rem;
-  @media (max-width: ${width.ipad}) {
-    text-align: left;
-  }
-`;
-
-const About = () => (
-  <>
-    <HeadComponent title="ABOUT" />
-    <BaseComponent heading="ABOUT">
-      <Author>
-        <AuthorIcon src="/about_icon.png" alt="tamaのアイコン" width="200" />
-        <AuthorName>tama</AuthorName>
-        <AuthorDescription>
-          a.k.a. Ryo Watanabe
-          <br />
-          平日は駆け出しエンジニア、週末は趣味でイラストやデザインをやっています。
-          <br />
-          音楽とカレーが好きです。
-        </AuthorDescription>
-      </Author>
-    </BaseComponent>
-  </>
-);
-
-export default About;
+export default AboutPage
