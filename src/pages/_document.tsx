e4daa2fca3c41f00028885f70f6dc40683b7b72c
ryo.watanabe <naberyo34@gmail.com>
2020-04-29 17:31:16 +0900

    fix: デプロイでコケるのを修正（多分）

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,22 +2,33 @@ import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
-  // TODO: 正直何やってるかわかってない styled-componentsをSSRで使うための処理
+// TODO: 仕方なかった
+type Props = {
+  styleElement: any;
+};
+
+export default class MyDocument extends Document<Props> {
+  /* TODO: 正直あまりわかってない styled-componentsをSSRで使うための処理
+    https://styled-components.com/docs/advanced#server-side-rendering
+  */
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
+    // ページコンポーネントからスタイルを取得
     const page = renderPage((App) => (props) =>
+      /* eslint-disable-next-line react/jsx-props-no-spreading */
       sheet.collectStyles(<App {...props} />)
     );
-    const styleTags = sheet.getStyleElement();
+    // スタイルタグを返す
+    const styleElement = sheet.getStyleElement();
 
-    return { ...page, styleTags };
+    return { ...page, styleElement };
   }
 
+  // getInitialPropsで生成したスタイルタグをheadに埋め込む
   render() {
     return (
       <Html lang="ja">
-        <Head>{this.props.styleTags}</Head>
+        <Head>{this.props.styleElement}</Head>
         <body>
           <Main />
           <NextScript />
